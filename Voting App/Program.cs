f5ab4d1a3cb465cf5226106a2ca1e8676b1b9d46
Dean-Fw <dfw662@gmail.com>
2021-03-01 18:07:26 +0000

    Classes made

---
+++ Voting App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Voting_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mepCanidates BP = new mepCanidates();
+            BP.partyname = "Brexit Party";
+            BP.voteCount = 452321;
+            BP.candidates = new string[5] {"BP1", "BP2", "BP3", "BP4", "BP5"};
+
+            mepCanidates LD = new mepCanidates();
+            LD.partyname = "Liberal Democrats";
+            LD.voteCount = 203989;
+            LD.candidates = new string[5] { "LD1", "LD2", "LD3", "LD4", "LD5" };
+
+            Console.WriteLine("Party: " + LD.partyname + " Vote Count: " + LD.voteCount.ToString());
+            for(int x = 0; x >= LD.candidates.Length; x++)
+            {
+                Console.WriteLine(LD.candidates[x]);
+            }
+
+
+        }
+    }
+}
---
+++ Voting App/mepCanidates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voting_App
+{
+    class mepCanidates
+    {
+        private string _partyName;
+        private int _voteCount;
+        private string[] _canidates;
+
+        public string partyname
+        {
+            get { return _partyName; }
+            set { _partyName = value; }
+        }
+
+        public int voteCount
+        {
+            get { return _voteCount; }
+            set { _voteCount = value; }
+        }
+        public string[] candidates
+        {
+            get { return _canidates; }
+            set { _canidates = value; }
+        }
+    }
+}
